92fc6c303e3905014e8556c9331b3b224e13b1fa
MesminN <mesmin.ngoulo@gmail.com>
2023-02-02 16:32:53 +0100

    Delete build-AIDS_Trojan-MVSC-Release directory

--- build-AIDS_Trojan-MVSC-Release/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/MSVC/14.34.31933/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
